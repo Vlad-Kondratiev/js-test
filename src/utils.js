89e7daef4ba107f31bca334db3ad4bbfee3e0286
Владимир Кондратьев <vlad.kondratieff@gmail.com>
2022-08-23 11:08:08 +0300

    230822

--- src/utils.js
+++ src/utils.js
@@ -142,24 +142,24 @@
 // Записать сумму в переменную totalSalary и вывести в консоль
 
 // 1. Сделать вары
-const minSalary = 500;
-const maxSalary = 5000;
-const employees = 3;
-let totalSalary = 0;
+// const minSalary = 500;
+// const maxSalary = 5000;
+// const employees = 3;
+// let totalSalary = 0;
 
 // 2. Перебрать работников в цикле
-for(let i = 1; i <= employees; i += 1) {
+// for(let i = 1; i <= employees; i += 1) {
 // 3. Сгенерить случайную з/п
-    const salary = Math.round(
-        Math.random() * (maxSalary - minSalary) + minSalary,
-    );
-console.log(`з/п работника номер ${i} - ${salary}`);
+//     const salary = Math.round(
+//         Math.random() * (maxSalary - minSalary) + minSalary,
+//     );
+// console.log(`з/п работника номер ${i} - ${salary}`);
 
 // 4. Прибавить к totalSalary
-totalSalary += salary;
-}
+// totalSalary += salary;
+// }
 // 5. Лог
-console.log('totalSalary: ', totalSalary);
+// console.log('totalSalary: ', totalSalary);
 
 // ===========================================================
 // Напиши скрипт, который подсчитывает сумму всех четных чисел
@@ -202,6 +202,8 @@ console.log('totalSalary: ', totalSalary);
 
 // console.log('total: ', total);
 
+// ==================================================
+
 // Напиши скрипт обработки покупки в магазине
 // - Баланс пользователя хранится в переменной balance
 // - Баланс покупки в payment
@@ -214,21 +216,66 @@ console.log('totalSalary: ', totalSalary);
 // - Вывести сообщение: "На счету недостаточно средств"
 // - Вывести сообщение: "Операция завершена"
 
-// 1. Вары
-let balance = 1500;
-const payment =2000;
+// let balance = 15000;
+// const payment = 2000;
 
-console.log(
-    `Общая стоимость заказа ${payment} кредитов. Проверяем кол-во средств на вашем счету`,
-);
+// console.log(
+//     `Общая стоимость заказа ${payment} кредитов. Проверяем кол-во средств на вашем счету`,
+// );
 
-if (balance >= payment) {
-    console.log('Ok');
+// if (balance >= payment) {
+//     console.log('Спасибо за покупку!');
 
-    balance -= payment;
+//     balance -= payment;
 
-        console.log(`На счету осталось ${balance} кредитов`);
-} else {
-        console.log('На счету недостаточно средств');
+//         console.log(`На счету осталось ${balance} кредитов`);
+// } else {
+//         console.log('На счету недостаточно средств');
+// }
+// console.log('Операция завершена');
+
+// ===========================================================
+
+// Напиши скрипт подсчета суммы покупки со скидкой в зависимости от потраченной суммы
+
+// Общая сумма потраченного хранится в totalSpent
+// Сумма текущего платежа в payment
+// Скидка хранится в discount
+// Если потрачено от 100 до 1000 - бронзовый партнер,скидка 2%
+// Если потрачено от 1000 до 5000 - серебрянный партнер,скидка 5%
+// Если потрачено более 5000 - золотой партнер,скидка 10%
+// Если потрачено менее 100 - не партнер,скидка 0%
+// В результате вывести сообщение:
+// Оформлен заказ на сумму (сумма) со скидкой (скидка)%
+
+let totalSpent = 3000;
+let payment = 550;
+let discount = 0;
+
+
+
+if(totalSpent >= 100 && totalSpent < 1000) {
+    console.log('Бронзовый партнер, скидка 2%');
+    discount = 0.02;
+}else if(totalSpent >= 1000 && totalSpent < 5000) {
+    console.log('Cеребрянный партнер, скидка 5%');
+    discount = 0.05;
+}else if (totalSpent >= 5000) {
+    console.log('Золотой партнер, скидка 10%');
+    discount = 0.1;
+}else {
+    console.log('У вас нет партнерской скидки');
 }
-console.log('Операция завершена');
+
+payment = payment - payment * discount;
+
+console.log(`Оформлен заказ на сумму ${payment} со скидкой ${discount * 100}%`);
+
+totalSpent += payment;
+
+console.log(`Общая сумма потраченного: ${totalSpent}`);
+
+// 'Бронзовый партнер, скидка 2%'
+// 'Cеребрянный партнер, скидка 5%'
+// 'Золотой партнер, скидка 10%'
+// 'У вас нет партнерской скидки'
