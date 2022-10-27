dc6cafe8872b10916dcfc82db00882a3a8651761
Владимир Кондратьев <vlad.kondratieff@gmail.com>
2022-10-27 11:14:59 +0300

    271022

--- src/modul3_1.js
+++ src/modul3_1.js
@@ -781,26 +781,272 @@
 
 //================= 20 =====================
 
-const products = [
-    { name: "Radar", price: 1300, quantity: 4 },
-    { name: "Scanner", price: 2700, quantity: 3 },
-    { name: "Droid", price: 400, quantity: 7 },
-    { name: "Grip", price: 1200, quantity: 9 },
-  ];
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
   
-  function calculateTotalPrice(productName) {
+//   function calculateTotalPrice(productName) {
+//   // перебираем объекты в массиве:
+//   for (const product of products) {
+//     // создаем переменную с общей стоимостью:
+//     let totalPrice = product.price * product.quantity;
+//     // если ключ name совпадает с productName:
+//     if (productName === product.name) {
+//       return totalPrice; 
+//     };
+//   };
+//   // если тело функции не выполняется возвращаем 0
+//   return 0;
+//   }
   
-  for (const product of products){
-  let totalPrice = product.price * product.quantity;
-    if (productName === product.name){
-      return totalPrice; 
-    }
+//   calculateTotalPrice("Blaster");
+//   calculateTotalPrice("Radar");
+//   calculateTotalPrice("Droid");
+//   calculateTotalPrice("Grip");
+//   calculateTotalPrice("Scanner");
+
+//================== 21 ========================  
+
+// const highTemperatures = {
+//     yesterday: 28,
+//     today: 26,
+//     tomorrow: 33,
+//   };
+  
+//   // const yesterday = highTemperatures.yesterday;
+//   // const today = highTemperatures.today;
+//   // const tomorrow = highTemperatures.tomorrow;
+//   const { yesterday, today, tomorrow } = highTemperatures;
+ 
+//   const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+//===================== 22 =======================
+
+// const highTemperatures = {
+//     yesterday: 28,
+//     today: 26,
+//     tomorrow: 33,
+//   };
+  
+//   // const yesterday = highTemperatures.yesterday;
+//   // const today = highTemperatures.today;
+//   // const tomorrow = highTemperatures.tomorrow;
+//   // const icon = highTemperatures.icon;
+  
+//   const {
+//     yesterday,
+//     today,
+//     tomorrow,
+//     icon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+//   } = highTemperatures;
+  
+//   const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+//===================== 23 ========================   
+
+// const highTemperatures = {
+//     yesterday: 28,
+//     today: 26,
+//     tomorrow: 33,
+//   };
+  
+//   // const highYesterday = highTemperatures.yesterday;
+//   // const highToday = highTemperatures.today;
+//   // const highTomorrow = highTemperatures.tomorrow;
+//   // const highIcon = highTemperatures.icon;
+  
+//   const {
+//     yesterday: highYesterday,
+//     today: highToday,
+//     tomorrow: highTomorrow,
+//     icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",  
+//   } = highTemperatures;
+  
+//   const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+//====================== 24 ========================
+
+// const colors = [
+//     { hex: "#f44336", rgb: "244,67,54" },
+//     { hex: "#2196f3", rgb: "33,150,243" },
+//     { hex: "#4caf50", rgb: "76,175,80" },
+//     { hex: "#ffeb3b", rgb: "255,235,59" },
+//   ];
+  
+//   const hexColors = [];
+//   const rgbColors = [];
+  
+//   for (const {hex, rgb} of colors) {
+  
+//     hexColors.push(hex);
+//     rgbColors.push(rgb);
+//   }
+
+//======================= 25 =======================
+
+// const forecast = {
+//     today: {
+//       low: 28,
+//       high: 32,
+//       icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+//     },
+//     tomorrow: {
+//       low: 27,
+//       high: 31,
+//     },
+//   };
+  
+//   // const highToday = forecast.today.high;
+// // const lowToday = forecast.today.low;
+// // const todayIcon = forecast.today.icon;
+
+// // const highTomorrow = forecast.tomorrow.high;
+// // const lowTomorrow = forecast.tomorrow.low;
+// // const tomorrowIcon = forecast.tomorrow.icon;
+
+//   const {
+//     today: { 
+//         low: lowToday, 
+//         high: highToday, 
+//         icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg" },
+//     tomorrow: { 
+//         low: lowTomorrow, 
+//         high: highTomorrow, 
+//         icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg" },
+//   } = forecast;
+  
+//======================= 26 =========================  
+
+// function calculateMeanTemperature(forecast) {
+//       // const todayLow = forecast.today.low;
+//       // const todayHigh = forecast.today.high;
+//       // const tomorrowLow = forecast.tomorrow.low;
+//       // const tomorrowHigh = forecast.tomorrow.high;
+//       // today: { low: 10, high: 20 },
+//       // tomorrow: { low: 20, high: 30 }
+  
+//     const {
+//       today: {
+//         low: todayLow, 
+//         high: todayHigh},
+//       tomorrow:{
+//         low: tomorrowLow, 
+//         high: tomorrowHigh},
+//      } = forecast;
+      
+//       return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+//     }
+
+//     calculateMeanTemperature({ 
+//         today: {low: 28, high: 32}, 
+//         tomorrow: {low: 25, high: 29} });
+//     calculateMeanTemperature({ 
+//         today: {low: 37, high: 40}, 
+//         tomorrow: {low: 33, high: 38} });
+
+//======================= 27 ==============================   
+
+// const scores = [89, 64, 42, 17, 93, 51, 26];
+
+// const bestScore = Math.max(...scores);
+// const worstScore = Math.min(...scores);
+
+//======================== 28 =============================
+
+const firstGroupScores = [64, 42, 93];
+const secondGroupScores = [89, 14, 51, 26];
+const thirdGroupScores = [29, 47, 18, 97, 81];
+
+const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+const bestScore = (Math.max(...allScores));
+const worstScore = (Math.min(...allScores));
+
+//======================== 29 ===========================
+
+// const defaultSettings = {
+//     theme: "light",
+//     public: true,
+//     withPassword: false,
+//     minNumberOfQuestions: 10,
+//     timePerQuestion: 60,
+//   };
+//   const overrideSettings = {
+//     public: false,
+//     withPassword: true,
+//     timePerQuestion: 30,
+//   };
+
+//   const finalSettings = {...defaultSettings, ...overrideSettings};
+//   console.log(finalSettings);
+
+//===================== 30 ==========================
+
+// function makeTask(data) {
+//     const completed = false;
+//     const category = "General";
+//     const priority = "Normal";
+      
+//     return { ...{ category, priority, completed }, ...data };
+// }
+  
+  
+//   makeTask({});
+//   makeTask({ category: "Homemade", priority: "Low", text: "Take out the trash" });
+//   makeTask({ category: "Finance", text: "Take interest" });
+//   makeTask({ priority: "Low", text: "Choose shampoo" });
+//   makeTask({ text: "Buy bread" });
+
+//========================= 31 ===========================
+
+function add(...args) {
+    let total = 0;
+    for (const arg of args) {
+     total += arg;
+    }    
+    return total;
   }
-  return 0;
+
+  add(15, 27);
+  add(12, 4, 11, 48);
+  add(32, 6, 13, 19, 8);
+  add(74, 11, 62, 46, 12, 36);
+
+//========================= 32 ==========================
+
+// function addOverNum(firstNum, ...args) {
+//     let total = 0;
+  
+//     for (const arg of args) {
+//       if (arg > firstNum)
+//       total += arg;
+//     }
+//     return total;
+//   }
+
+//   addOverNum(50, 15, 27);
+//   addOverNum(10, 12, 4, 11, 48, 10, 8);
+//   addOverNum(15, 32, 6, 13, 19, 8);
+//   addOverNum(20, 74, 11, 62, 46, 12, 36);
+
+//========================== 33 ==========================
+
+function findMatches(numbers, ...args) {
+    const matches = []; // Don't change this line
+  for (const number of numbers) {
+    if(args.includes(number)) {
+        matches.push(number);   
+    } 
+  }
+    return matches;
   }
   
-  calculateTotalPrice("Blaster");
-  calculateTotalPrice("Radar");
-  calculateTotalPrice("Droid");
-  calculateTotalPrice("Grip");
-  calculateTotalPrice("Scanner");
+  findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7);
+  findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2);
+  findMatches([10, 24, 41, 6, 9, 19], 24, 11, 9, 23, 41);
+  findMatches([63, 11, 8, 29], 4, 7, 16);
+
+//========================== 34 ===========================  
+
